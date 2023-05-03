9e7856b84637132761b8972c80f2258ccf8aa312
daniellfranco <daniel.luis.franco@gmail.com>
2023-05-02 22:01:44 -0300

    feat: Cria classes da camada CrossCutting

---
+++ Catalogo/Catalogo.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using Catalogo.Application.Interfaces;
+using Catalogo.Application.Mappings;
+using Catalogo.Application.Services;
+using Catalogo.Domain.Interfaces;
+using Catalogo.Infrastructure.Context;
+using Catalogo.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalogo.CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ),
+            // Excencial para não dar erro no migrations
+            b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
